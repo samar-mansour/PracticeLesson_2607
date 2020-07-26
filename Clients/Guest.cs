b3d8f0c42e24d8041f70bf24e79d41dac727cda7
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-07-26 21:19:00 +0300

    Add files via upload

---
+++ Clients/Guest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    public class Guest
+    {
+        private int id;
+        private string name;
+
+        public int GetId()
+        {
+            return this.id;
+        } 
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public Guest(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+
+    }
+}

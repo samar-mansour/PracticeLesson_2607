d1d0600f134ef9270d16ed579df920b5a7032ee7
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-07-26 21:20:11 +0300

    Add files via upload

---
+++ Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Clients;
+using Hotel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLesson2607
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guest guest = new Guest(123456,"Gary");
+            Reception.CheckIn(guest);
+        }
+    }
+}
